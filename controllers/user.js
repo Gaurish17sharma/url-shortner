57d77e08a0564271b61f92ebac5cd4edcabe44de
Gaurish17Sharma <gaurishsharma03@gmail.com>
2024-06-07 13:17:29 +0530

    sign in

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,11 @@
 const pool = require('../configs/database');
+const  bcrypt  =  require("bcrypt");
+const  jwt  =  require("jsonwebtoken");
+
+const secret = "Admin@123";
 
 const handleCreateSignUp = async (req,res) => {
     const {name , email , password} = req.body;
-    const  user = {
-        name ,
-        email,
-        password,
-        };
 
     pool.query(' SELECT * from users WHERE email = $1 ' , [email], (err,results) => {
         console.log("a");
@@ -21,16 +20,45 @@ const handleCreateSignUp = async (req,res) => {
                 });
         }
         else{
-            return res.json({
-                message: "User successfully created",
-            user,});
+            bcrypt.hash(password , 10, (err,hash) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                const  user = {
+                    name: name ,
+                    email: email,
+                    password: hash,
+                    };
+
+                var flag = 1;
+
+                pool.query('INSERT INTO user ( name , email , password ) VALUES ($1 , $2 , $3)' ,  [user.name, user.email, user.password] , (err) => {
+                    if (err){
+                        flag = 0;
+                        console.log(err);
+                        return  res.status(500).json({
+                            error: "Database error"
+                        })        
+                    }
+                    else{
+                        flag = 1;
+                        res.status(200).send({ message: 'User added to database' });
+                        console.log("User Successfully Created");
+                    }
+                })
+
+                if (flag) {
+                    const token = jwt.sign(
+                        {
+                            email: user.email
+                        },
+                        secret               
+                );
+                };
+            })
         }
     })
-
-}
-
-const handleCreatelogin = (req,res) =>{
-    const { email , password} = req.body;
 
 }
 
